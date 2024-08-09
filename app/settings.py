d298ac90e674156c57dba3e23aa7108e7791c4c8
HarshSonawane <sonawaneharshwardhan@gmail.com>
2024-08-10 02:14:53 +0530

    updated settings to use aws

--- app/settings.py
+++ app/settings.py
@@ -41,6 +41,7 @@ THIRD_PARTY_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "corsheaders",
+    "storages",
 ]
 
 LOCAL_APPS = [
@@ -151,17 +152,36 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+#
 
-STATIC_URL = "/static/"
+USE_S3 = env("USE_S3")
+if USE_S3:
+    AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_OBJECT_PARAMETERS = {
+        "CacheControl": "max-age=86400",
+    }
+    AWS_LOCATION = "static"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
-]
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+    DEFAULT_FILE_STORAGE = "app.storage_backends.MediaStorage"
 
-STATIC_ROOT = BASE_DIR / "static"
+else:
+    STATIC_URL = "/static/"
 
-MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_URL = "/media/"
+    STATICFILES_DIRS = [
+        BASE_DIR / "staticfiles",
+    ]
+
+    STATIC_ROOT = BASE_DIR / "static"
+
+    MEDIA_ROOT = BASE_DIR / "media"
+    MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
