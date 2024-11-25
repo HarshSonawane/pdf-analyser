fcc3c5f9241a25bd434cf822573e302898b45ba6
HarshSonawane <sonawaneharshwardhan@gmail.com>
2024-11-25 21:35:14 +0530

    commening the unused class

--- core/models.py
+++ core/models.py
@@ -8,7 +8,7 @@ from django.core.files.base import ContentFile
 import boto3
 import tempfile
 from io import BytesIO
-import fitz
+# import fitz
 import re
 from services.plumber_analyzer import PlumberAnalyzer
 
@@ -120,148 +120,148 @@ class PageResult(BaseModel):
         verbose_name_plural = "Page Results"
 
 
-class FitsAnalyzer:
-    def __init__(self, pdf_path, review_request):
-        self.pdf = fitz.open(pdf_path)
-        self.analysis = {}
-        self.analyse()
-        try:
-            self.review_request = ReviewRequest.objects.get(id=review_request)
-        except ReviewRequest.DoesNotExist:
-            self.review_request = None
-            return None
+# class FitsAnalyzer:
+#     def __init__(self, pdf_path, review_request):
+#         self.pdf = fitz.open(pdf_path)
+#         self.analysis = {}
+#         self.analyse()
+#         try:
+#             self.review_request = ReviewRequest.objects.get(id=review_request)
+#         except ReviewRequest.DoesNotExist:
+#             self.review_request = None
+#             return None
 
-        self.save_results(review_request)
+#         self.save_results(review_request)
 
-    def save_results(self, review_request_id):
-        for page_num, details in self.analysis.items():
-            matches_margins = self.match_margins(details)
-            details["matches_margins"] = matches_margins
-            PageResult.objects.create(
-                review_request_id=review_request_id,
-                page_number=page_num,
-                service="fitz",
-                details=details,
-                flaged=(not matches_margins),
-            )
+#     def save_results(self, review_request_id):
+#         for page_num, details in self.analysis.items():
+#             matches_margins = self.match_margins(details)
+#             details["matches_margins"] = matches_margins
+#             PageResult.objects.create(
+#                 review_request_id=review_request_id,
+#                 page_number=page_num,
+#                 service="fitz",
+#                 details=details,
+#                 flaged=(not matches_margins),
+#             )
 
-    def match_margins(self, details):
-        if (
-            self.review_request.top_margin
-            == self.points_to_inches(details["margins"]["top"])
-            and self.review_request.bottom_margin
-            == self.points_to_inches(details["margins"]["bottom"])
-            and self.review_request.left_margin
-            == self.points_to_inches(details["margins"]["left"])
-            and self.review_request.right_margin
-            == self.points_to_inches(details["margins"]["right"])
-        ):
-            return True
-        return False
+#     def match_margins(self, details):
+#         if (
+#             self.review_request.top_margin
+#             == self.points_to_inches(details["margins"]["top"])
+#             and self.review_request.bottom_margin
+#             == self.points_to_inches(details["margins"]["bottom"])
+#             and self.review_request.left_margin
+#             == self.points_to_inches(details["margins"]["left"])
+#             and self.review_request.right_margin
+#             == self.points_to_inches(details["margins"]["right"])
+#         ):
+#             return True
+#         return False
 
-    def points_to_inches(self, points):
-        return points / 72
+#     def points_to_inches(self, points):
+#         return points / 72
 
-    def analyse(self):
-        num_pages = self.pdf.page_count
-        for page_num in range(num_pages):
-            page = self.pdf.load_page(page_num)
-            margins = self.get_page_margins(page)
-            self.analysis[page_num] = {
-                "margins": margins,
-                "is_blank": self.is_blank_page(page, page_num),
-                "is_single_side": self.is_single_side_page(margins),
-                "is_double_side": self.is_double_side_page(page_num, margins),
-                "side": "single" if self.is_single_side_page(margins) else "double",
-                "is_page_numbered": self.is_page_numbered(page),
-                "page_number_coordinates": self.page_numbers_and_coordinates(page),
-                "is_landscaped": self.is_landscaped(page),
-                "is_portraited": self.is_portraited(page),
-                "orientation": "landscape" if self.is_landscaped(page) else "portrait",
-                "text_percentage": self.get_text_percentage(page),
-            }
+#     def analyse(self):
+#         num_pages = self.pdf.page_count
+#         for page_num in range(num_pages):
+#             page = self.pdf.load_page(page_num)
+#             margins = self.get_page_margins(page)
+#             self.analysis[page_num] = {
+#                 "margins": margins,
+#                 "is_blank": self.is_blank_page(page, page_num),
+#                 "is_single_side": self.is_single_side_page(margins),
+#                 "is_double_side": self.is_double_side_page(page_num, margins),
+#                 "side": "single" if self.is_single_side_page(margins) else "double",
+#                 "is_page_numbered": self.is_page_numbered(page),
+#                 "page_number_coordinates": self.page_numbers_and_coordinates(page),
+#                 "is_landscaped": self.is_landscaped(page),
+#                 "is_portraited": self.is_portraited(page),
+#                 "orientation": "landscape" if self.is_landscaped(page) else "portrait",
+#                 "text_percentage": self.get_text_percentage(page),
+#             }
 
-    def get_page_margins(self, page):
-        page_rect = page.rect
-        page_width = page_rect.width
-        page_height = page_rect.height
+#     def get_page_margins(self, page):
+#         page_rect = page.rect
+#         page_width = page_rect.width
+#         page_height = page_rect.height
 
-        text_blocks = page.get_text("blocks")
+#         text_blocks = page.get_text("blocks")
 
-        # Initialize margins
-        top_margin = page_height
-        bottom_margin = page_height
-        left_margin = page_width
-        right_margin = page_width
+#         # Initialize margins
+#         top_margin = page_height
+#         bottom_margin = page_height
+#         left_margin = page_width
+#         right_margin = page_width
 
-        # Calculate the margins
-        for block in text_blocks:
-            block_rect = fitz.Rect(block[:4])
+#         # Calculate the margins
+#         for block in text_blocks:
+#             block_rect = fitz.Rect(block[:4])
 
-            top_margin = min(top_margin, block_rect.y0)
-            bottom_margin = min(bottom_margin, page_height - block_rect.y1)
-            left_margin = min(left_margin, block_rect.x0)
-            right_margin = min(right_margin, page_width - block_rect.x1)
+#             top_margin = min(top_margin, block_rect.y0)
+#             bottom_margin = min(bottom_margin, page_height - block_rect.y1)
+#             left_margin = min(left_margin, block_rect.x0)
+#             right_margin = min(right_margin, page_width - block_rect.x1)
 
-        return {
-            "top": top_margin,
-            "bottom": bottom_margin,
-            "left": left_margin,
-            "right": right_margin,
-        }
+#         return {
+#             "top": top_margin,
+#             "bottom": bottom_margin,
+#             "left": left_margin,
+#             "right": right_margin,
+#         }
 
-    def is_blank_page(self, page, page_num):
-        text_blocks = page.get_text("blocks")
-        image_list = page.get_images(full=True)
-        # remove tabs and newlines
-        text_blocks = [
-            block[4].replace("\n", "").replace("\t", "") for block in text_blocks
-        ]
+#     def is_blank_page(self, page, page_num):
+#         text_blocks = page.get_text("blocks")
+#         image_list = page.get_images(full=True)
+#         # remove tabs and newlines
+#         text_blocks = [
+#             block[4].replace("\n", "").replace("\t", "") for block in text_blocks
+#         ]
 
-        if page_num == 3:
-            breakpoint()
+#         if page_num == 3:
+#             breakpoint()
 
-        if not text_blocks and not image_list:
-            return True
-        return False
+#         if not text_blocks and not image_list:
+#             return True
+#         return False
 
-    def is_single_side_page(self, margins):
-        # if left margin and right margin are equal then it is a single side page
-        if margins["left"] == margins["right"]:
-            return True
+#     def is_single_side_page(self, margins):
+#         # if left margin and right margin are equal then it is a single side page
+#         if margins["left"] == margins["right"]:
+#             return True
 
-    def is_double_side_page(self, page_num, margins):
-        ## if page number is even then the left margine should be greater than right margin
-        if page_num % 2 == 0 and margins["left"] > margins["right"]:
-            return True
+#     def is_double_side_page(self, page_num, margins):
+#         ## if page number is even then the left margine should be greater than right margin
+#         if page_num % 2 == 0 and margins["left"] > margins["right"]:
+#             return True
 
-    def is_page_numbered(self, page):
-        text = page.get_text()
-        lat_lon_pattern = re.compile(r"([-+]?\d{1,2}\.\d+),\s*([-+]?\d{1,3}\.\d+)")
-        matches = lat_lon_pattern.findall(text)
-        return bool(matches)
+#     def is_page_numbered(self, page):
+#         text = page.get_text()
+#         lat_lon_pattern = re.compile(r"([-+]?\d{1,2}\.\d+),\s*([-+]?\d{1,3}\.\d+)")
+#         matches = lat_lon_pattern.findall(text)
+#         return bool(matches)
 
-    def page_numbers_and_coordinates(self, page):
-        lat_lon_pattern = re.compile(r"([-+]?\d{1,2}\.\d+),\s*([-+]?\d{1,3}\.\d+)")
-        text = page.get_text()
-        matches = lat_lon_pattern.findall(text)
-        return matches
+#     def page_numbers_and_coordinates(self, page):
+#         lat_lon_pattern = re.compile(r"([-+]?\d{1,2}\.\d+),\s*([-+]?\d{1,3}\.\d+)")
+#         text = page.get_text()
+#         matches = lat_lon_pattern.findall(text)
+#         return matches
 
-    def is_landscaped(self, page):
-        page_rect = page.rect
-        return page_rect.width > page_rect.height
+#     def is_landscaped(self, page):
+#         page_rect = page.rect
+#         return page_rect.width > page_rect.height
 
-    def is_portraited(self, page):
-        page_rect = page.rect
-        return page_rect.width < page_rect.height
+#     def is_portraited(self, page):
+#         page_rect = page.rect
+#         return page_rect.width < page_rect.height
 
-    def get_text_percentage(self, page):
-        text = page.get_text()
-        page_rect = page.rect
-        text_area = 0
-        for block in page.get_text("blocks"):
-            block_rect = fitz.Rect(block[:4])
-            text_area += block_rect.width * block_rect.height
+#     def get_text_percentage(self, page):
+#         text = page.get_text()
+#         page_rect = page.rect
+#         text_area = 0
+#         for block in page.get_text("blocks"):
+#             block_rect = fitz.Rect(block[:4])
+#             text_area += block_rect.width * block_rect.height
 
-        page_area = page_rect.width * page_rect.height
-        return (text_area / page_area) * 100
+#         page_area = page_rect.width * page_rect.height
+#         return (text_area / page_area) * 100
